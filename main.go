ad6306cf2e1339ed6634c397a3cb4a6d6cffb3ca
Milo Gertjejansen <milo@milogert.com>
2014-11-16 13:12:46 -0500

    Modified: Added bad code.

--- main.go
+++ main.go
@@ -3,13 +3,48 @@ package main
 import (
   "github.com/go-martini/martini"
   "github.com/martini-contrib/render"
-  "code.google.com/p/go-sqlite/go1/sqlite3"
+  "github.com/martini-contrib/sessions"
+  "github.com/mattn/go-sqlite3"
   "time"
   "encoding/json"
+  "os"
+  "os/exec"
+  "fmt"
 )
 
+func resetDb() {
+  // Remove the old database.
+  fmt.Println("ii Removing the old database.")
+  os.Remove("./db.db")
+
+  // Re-create the database.
+  fmt.Println("ii Re-creating the database.")
+  cmd := exec.Command("/usr/bin/sqlite3", "ocdns.db", "<", "db.sql")
+  cmd.Stdin = os.Stdin
+  cmd.Stdout = os.Stdout
+  cmd.Stderr = os.Stderr
+  err := cmd.Run()
+
+  // Check the output.
+  if err != nil {
+    fmt.Println("-- Failed to re-create the database.")
+    //fmt.Println("Output: " + err)
+    os.Exit(1)
+  }
+
+  fmt.Println("ii Successfully created the new file.")
+}
+
 func main() {
+  // Reset the database.
+  resetDb()
+
+  // Get the Martini instance.
   m := martini.Classic()
+
+  // Create session store.
+  store := sessions.NewCookieStore([]byte("doublesecret"))
+  m.Use(sessions.Sessions("ocdns", store))
 
   // Set a layout.
   m.Use(render.Renderer(render.Options{
@@ -19,21 +54,27 @@ func main() {
   m.Get("/", func(r render.Render) {
     // TODO: Perform checks to see if the user is logged in. If they are, then
     // render a specific page based on their role.
-    r.HTML(200, "index", "")
-  })
+    role := session.Get("role")
 
-  m.Get("/login", func(r render.Render) {
-    // Do stuff.
-
-    // TODO: Check what the user's role and return a page depending upon that.
-
-    // r.HTML(200, "/admin", "")
-    // r.HTML(200, "/judge", "")
-    // r.HTML(200, "/contestant", "")
+    if role == "Admin" {
+      r.HTML(302, "admin", "")
+    } else if role == "Judge" {
+      r.HTML(302, "judge", "")
+    } else if role == "Player" {
+      r.HTML(302, "player", "")
+    } else {
+      r.HTML(200, "index", "")
+    }
   })
 
   m.Get("/judge", func(r render.Render) {
-    r.HTML(200, "judge", "test")
+    role := session.Get("role")
+
+    if role == "Judge" {
+      r.HTML(200, "judge", "test")
+    } else {
+      r.HTML(302, "index", "test")
+    }
   })
 
   m.Get("/api/getSubmissions", func(r render.Render) {
@@ -51,7 +92,7 @@ func main() {
     // Return the results in a json object.
     var aRet map[string]RowMap
     aRow := make(sqlite3.RowMap)
-    for aRes, aErr, aErr == nil; err = aRes.next() {
+    for aRes.Next() {
       // var aId int64
       // var aSubmitTime time
       // var aSubmit string
